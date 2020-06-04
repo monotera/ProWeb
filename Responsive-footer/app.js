db7c020cb9e57620350467cc22c530e14d58fc9c
Nelson_Mosquera <nelson.a.mosquera@gmail.com>
2020-06-04 13:50:45 -0500

    Creates a responive footer

---
+++ Responsive-footer/app.js
@@ -0,0 +1,9 @@
+const footerList = document.querySelector(".footer-wrapper-ul");
+const footerLinks = footerList.querySelectorAll("a");
+for (let i = 0; i < footerLinks.length; i++) {
+  footerLinks[i].addEventListener("click", () => {
+    let current = footerList.querySelector(".active");
+    current.className = "footer-a";
+    footerLinks[i].className = "active";
+  });
+}
