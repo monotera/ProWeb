765868f89dd92e7a7a2b606aa47bb8d32c6a3d22
monotera <nelson.a.mosquera@gmail.com>
2020-12-09 01:59:08 -0500

    Creates twotter

---
+++ Vue/twotter/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <nav class="app__nav">
+      <h1 class="nav__logo">Twotter</h1>
+      <h2 class="nav__user">@{{ user.username }}</h2>
+    </nav>
+    <user-profile />
+  </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile.vue";
+export default {
+  name: "App",
+  components: { UserProfile },
+  data() {
+    {
+      return {
+        user: {
+          username: "Monotera",
+        },
+      };
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #f3f5fa;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  min-height: 100vh;
+}
+.app__nav {
+  align-items: center;
+  background-color: lightseagreen;
+  color: white;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: auto;
+  min-height: 10vh;
+  padding: 10px 5%;
+
+  .nav__logo {
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+  .nav__user {
+    font-weight: bold;
+  }
+}
+</style>
---
+++ Vue/twotter/src/components/UserProfile.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__sidebar">
+      <div class="user-profile__user-panel">
+        <h1 class="user-profile__username">@{{ user.username }}</h1>
+        <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+        <div class="user-profile__admin-badge" v-if="!user.isAdmin">User</div>
+        <div class="user-profile__follower-count">
+          <strong>followers: </strong> {{ followers }}
+        </div>
+      </div>
+      <create-twoot-panel @add-twoot="addTwoot" />
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <twoot-item
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favourite="toggleFavorite"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import CreateTwootPanel from "./CreateTwootPanel.vue";
+import TwootItem from "./TwootItem.vue";
+export default {
+  components: { TwootItem, CreateTwootPanel },
+  name: "UserProfile",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "monotera",
+        fistName: "Nelson",
+        lastName: "Mosquera",
+        email: "email@gmail.com",
+        isAdmin: false,
+        twoots: [
+          { id: 1, content: "My first Twoot" },
+          { id: 2, content: "Hello world" },
+        ],
+      },
+    };
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} gained a folower`);
+      }
+    },
+  },
+  computed: {},
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+    toggleFavorite(id) {
+      console.log(`favoutied twoot ${id}`);
+    },
+    addTwoot(twoot) {
+      this.user.twoots.unshift({
+        id: this.user.twoots.length + 1,
+        content: twoot,
+      });
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 50px;
+  padding: 50px 5%;
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
+    margin-bottom: auto;
+    h1 {
+      margin: 0;
+      padding-bottom: 0.4rem;
+    }
+    .user-profile__follower-count {
+      padding-top: 1rem;
+    }
+    .user-profile__admin-badge {
+      background: rebeccapurple;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0.4rem 1rem;
+      font-weight: bold;
+    }
+  }
+  .user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+    margin-bottom: auto;
+  }
+}
+</style>
