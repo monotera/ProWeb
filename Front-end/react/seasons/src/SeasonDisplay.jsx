2d5e9f5fb3c0ad44df75a86ab83a3c51dfff842a
monotera <nelson.a.mosquera@gmail.com>
2021-06-11 20:26:29 -0500

    Section 6

--- Front-end/react/seasons/src/SeasonDisplay.jsx
+++ Front-end/react/seasons/src/SeasonDisplay.jsx
@@ -1,7 +1,38 @@
+import "./SeasonDisplay.css";
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Bruh</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Buuuurrrr, It is cold!!!",
+    iconNAme: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <h1>
+        <i className={`icon-left massive ${iconName} icon`} />
+        {text}
+        <i className={`icon-right massive ${iconName} icon`} />
+      </h1>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- Front-end/react/seasons/src/index.js
+++ Front-end/react/seasons/src/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import SeasonDisplay from "./SeasonDisplay";
+import Loader from "./Loader";
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMsg: "" };
+  state = { lat: null, errorMsg: "" };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMsg: err.message });
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMsg: err.message })
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMsg && !this.state.lat)
       return <div>Error : {this.state.errorMsg}</div>;
     else if (!this.state.errorMsg && this.state.lat)
-      return <div>Latitude: {this.state.lat}</div>;
-    return <div>Loading</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
+    return <Loader msg="Allow the location Dummy!" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
