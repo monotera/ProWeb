e8a7a92f0c08f9a1bc5d5d0cf4e40a8f340e441d
monotera <nelson.a.mosquera@gmail.com>
2021-06-15 16:12:02 -0500

    Section10

--- Front-end/react/pics/src/components/ImageList.jsx
+++ Front-end/react/pics/src/components/ImageList.jsx
@@ -1,10 +1,12 @@
+import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = (props) => {
-  const images = props.imgs.map(({ description, urls, id }) => {
-    return <img key={id} alt={description} src={urls.regular} />;
+  const images = props.imgs.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
