c3c85dcbf6a49fea51f71101efbdb737c8647544
Monotera <nelson.a.mosquera@gmail.com>
2021-06-24 20:04:57 -0500

    section 11

--- Front-end/react/videos/src/components/App.js
+++ Front-end/react/videos/src/components/App.js
@@ -2,27 +2,45 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../API/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./videoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
   onTermSubmit = async (term) => {
     const { data } = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({ videos: data.items });
+    this.setState({
+      videos: data.items,
+      selectedVideo: data.items[0],
+    });
   };
   onVideoSelect = (v) => {
-    console.log("Fomr the app!", v);
+    this.setState({ selectedVideo: v });
   };
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
