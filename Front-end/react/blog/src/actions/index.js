de4769b20d9a3d2413dda21fe2ffb056c9b86d87
monotera <nelson.a.mosquera@gmail.com>
2021-07-21 22:33:15 -0500

    Section 18

---
+++ Front-end/react/blog/src/actions/index.js
@@ -0,0 +1,6 @@
+import jsonPlaceHolder from "../API/jsonPlaceHolder";
+
+export const fetchPosts = () => async(dispatch) => {
+    const response = await jsonPlaceHolder.get("posts");
+    dispatch({ type: "FETCH_POSTS", payload: response });
+};
