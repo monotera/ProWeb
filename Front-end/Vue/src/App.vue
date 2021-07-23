162b009f82cf800bc9d8c9e80f6a604120b59f20
monotera <nelson.a.mosquera@gmail.com>
2021-07-22 20:50:19 -0500

    Merge repositories

---
+++ Front-end/Vue/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <nav class="app__nav">
+      <router-link to="/"><h1 class="nav__logo">Twotter</h1> </router-link>
+      <a class = "nav__user"  v-if="user" href="https://github.com/monotera">@{{ user.username }}</a>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const store = useStore();
+    const user = computed(() => store.state.User.user);
+    return {
+      user,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #f3f5fa;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  min-height: 100vh;
+}
+.app__nav {
+  align-items: center;
+  background-color: lightseagreen;
+  color: white;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: auto;
+  min-height: 10vh;
+  padding: 10px 5%;
+
+  .nav__logo {
+    font-size: 1.8rem;
+    font-weight: bold;
+  }
+  .nav__user {
+    font-weight: bold;
+    font-size: 1.7rem;
+  }
+}
+</style>
---
+++ Front-end/Vue/src/views/Home.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="home">
+    <div class="home__greetings">
+      <h1>Welcome to <strong class="home__twotter">Twotter!</strong></h1>
+      <h2>Choose your user:</h2>
+    </div>
+    <div class="users-list">
+      <router-link
+        class="home__username"
+        v-for="user in users"
+        :to="{ name: 'UserProfile', params: { userId: user.id } }"
+        :key="user.id"
+        >{{ user.username }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import { users } from "../assets/users";
+export default {
+  name: "Home",
+  setup() {
+    return {
+      users,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  min-height: 80vh;
+  @media screen and (min-width: 768px) {
+    align-items: center;
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+  }
+
+  .home__greetings {
+    font-size: 1rem;
+    margin: 2.5rem;
+    text-align: center;
+    @media screen and (min-width: 768px) {
+      font-size: 2.5rem;
+    }
+  }
+
+  .home__twotter {
+    color: lightseagreen;
+  }
+  .users-list {
+    display: flex;
+    flex-flow: column wrap;
+    @media screen and (min-width: 768px) {
+      flex-flow: row;
+    }
+  }
+  .home__username {
+    background-color: lightpink;
+    border: 1px solid black;
+    border-radius: 5px;
+    font-size: 1.5rem;
+    margin: 1rem;
+    padding: 0.5rem;
+    text-align: center;
+    transition: all 0.4s ease;
+    &:hover {
+      background-color: lightseagreen;
+      color: white;
+    }
+  }
+}
+</style>
