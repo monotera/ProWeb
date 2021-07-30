79797b06c686ba31dc68a3c2586e4010c8af29a0
monotera <nelson.a.mosquera@gmail.com>
2021-07-29 19:10:13 -0500

    FailSafe

---
+++ Front-end/Javeriana-Proweb/Portfolio/app.js
@@ -0,0 +1,18 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.use("/*", (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "views", "Home.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
