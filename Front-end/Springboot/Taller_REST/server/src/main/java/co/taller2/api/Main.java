d500143802500ddec7f7f1ed7e9069294c2b4394
monotera <nelson.a.mosquera@gmail.com>
2021-09-04 03:01:57 -0500

    failsafe

--- Front-end/Springboot/Taller_REST/server/src/main/java/co/taller2/api/Main.java
+++ Front-end/Springboot/Taller_REST/server/src/main/java/co/taller2/api/Main.java
@@ -11,15 +11,9 @@ import java.net.URI;
  * Main class. mvn clean install mvn exec::java
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
+
     public static final String BASE_URI = "http://localhost:3000/";
 
-    /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this
-     * application.
-     * 
-     * @return Grizzly HTTP server.
-     */
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in co.taller2.api package
@@ -30,12 +24,6 @@ public class Main {
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-    /**
-     * Main method.
-     * 
-     * @param args
-     * @throws IOException
-     */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format(
